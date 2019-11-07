b61d334e48ed51f7d19ddea1b3af62f0f40eb1b4
Martin Zielinski <darthcoder1@gmail.com>
2019-11-08 00:29:20 +0100

    Massaged Dockerfile
    * upgraded Dockerfile to ubuntu 18.04 and cleaned it up
    * added additional packages to be preinstalled
    * removed android specific commands from launch script (for now)
    * yaakified the Dockerfile (pull from yaak repos)

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -12,15 +12,7 @@ fi
 
 function launch {
 
-  # no cpu rationing for now
-  echo 0-3 > /dev/cpuset/background/cpus
-  echo 0-3 > /dev/cpuset/system-background/cpus
-  echo 0-3 > /dev/cpuset/foreground/boost/cpus
-  echo 0-3 > /dev/cpuset/foreground/cpus
-  echo 0-3 > /dev/cpuset/android/cpus
-
   # handle pythonpath
-  ln -s /data/openpilot /data/pythonpath
   export PYTHONPATH="$PWD"
 
   # start manager
