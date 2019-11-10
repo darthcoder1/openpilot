e0116e55d9ef95c38a1d8a44516804a846b08f2a
Martin Zielinski <darthcoder1@gmail.com>
2019-11-10 15:45:56 +0100

    add installation script for external deps

---
+++ external/install_external_deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install tensorflow
+TENSORFLOW_FILENAME=libtensorflow-gpu-linux-x86_64-1.14.0.tar.gz
+mkdir tensorflow
+wget https://storage.googleapis.com/tensorflow/libtensorflow/$TENSORFLOW_FILENAME
+
+tar -C tensorflow/ -xzf $TENSORFLOW_FILENAME
+rm $TENSORFLOW_FILENAME
